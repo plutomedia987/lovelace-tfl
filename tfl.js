573df1d98875980e5a1658c1948980f7ada0828e
plutomedia987 <daniel@plutomedia.net>
2025-03-15 15:37:41 +0000

    Added a local copy of lit to stop CORS errors

--- tfl.js
+++ tfl.js
@@ -2,7 +2,7 @@ import {
     LitElement,
     html,
     css,
-  } from "https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
+  } from "./lit-core.min.js"; //"https://unpkg.com/lit-element@2.0.1/lit-element.js?module";
 
 
   const fireEvent = (node, type, detail, options) => {
